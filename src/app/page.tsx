9a308d110fbc3add32273cedf5b5882906c576a5
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2025-02-02 20:10:49 -0300

    Primeiro Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Projeto Dashboard</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
